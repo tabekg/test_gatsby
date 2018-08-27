34c56142cc107a9840427f5c51ee94c602e3a04c
Bekbolot <tabekg@gmail.com>
2018-08-27 20:27:45 +0600

    contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Test',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+  	'gatsby-plugin-react-helmet',
+	  	{
+	    resolve: `gatsby-source-contentful`,
+	    options: {
+	      spaceId: `m6gv0asl720l`,
+	      accessToken: `aa6fcbc07a5339482bdd0e1a7a5f460d8b75a9f0d21f4faf884195bcd655b38b`,
+	    },
+	  }],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>{data.contentfulMydata.title}</h1>
+    <p>{data.contentfulMydata.description.description}</p>
+    <img src={data.contentfulMydata.image.resolutions.src} />
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
 
 export default IndexPage
+
+export const query = graphql`
+	query PageQuery {
+		contentfulMydata {
+	    title
+	    description {
+	      id
+	      description
+	    }
+	    image {
+	      id
+	      resolutions(width:300, height:200, resizingBehavior:THUMB){
+	        src
+	      }
+	    }
+	  }
+	}
+`
